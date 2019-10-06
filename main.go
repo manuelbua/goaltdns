44a03f6a04be68701419eb483f61f4b84eedd30d
Manuel Bua <manuel.bua@gmail.com>
2019-10-06 20:59:05 +0200

    Add back file output, add verbosity flag, cleanup

--- main.go
+++ main.go
@@ -8,20 +8,30 @@ import (
 	"os"
 	"sync"
 	// "time"
-	// "runtime"
+	"runtime"
 
 	"github.com/bobesa/go-domain-util/domainutil"
 	"github.com/subfinder/goaltdns/altdns"
 	"github.com/subfinder/goaltdns/util"
 )
 
+func loge(text string, args ...interface{}) {
+	if len(args) == 0 {
+		fmt.Fprintf(os.Stderr, text)
+	} else {
+		fmt.Fprintf(os.Stderr, text, args...)
+	}
+}
+
 func main() {
-	var wordlist, host, list/*, output*/ string
-	hostList := []string{}
+	var wordlist, host, list, output string
+	var verbose bool
+
 	flag.StringVar(&host, "h", "", "Host to generate permutations for")
 	flag.StringVar(&list, "l", "", "List of hosts to generate permutations for")
 	flag.StringVar(&wordlist, "w", "words.txt", "Wordlist to generate permutations with")
-	// flag.StringVar(&output, "o", "", "File to write permutation output to (optional)")
+	flag.StringVar(&output, "o", "", "File to write permutation output to (optional)")
+	flag.BoolVar(&verbose, "v", false, "Enable verbosity")
 
 	flag.Parse()
 
@@ -30,6 +40,7 @@ func main() {
 		os.Exit(1)
 	}
 
+	hostList := []string{}
 	if host != "" {
 		hostList = append(hostList, host)
 	}
@@ -42,17 +53,18 @@ func main() {
 		hostList = append(hostList, util.LinesInStdin()...)
 	}
 
-	// var f *os.File
-	var err error
-	// if output != "" {
-	// 	f, err = os.OpenFile(output, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0600)
-	// 	if err != nil {
-	// 		fmt.Printf("output: %s\n", err)
-	// 		os.Exit(1)
-	// 	}
+	var outfile *os.File = os.Stdout
 
-	// 	defer f.Close()
-	// }
+	var err error
+	if output != "" {
+		outfile, err = os.OpenFile(output, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0600)
+		if err != nil {
+			fmt.Printf("output: %s\n", err)
+			os.Exit(1)
+		}
+
+		defer outfile.Close()
+	}
 
 	altdns, err := altdns.New(wordlist)
 	if err != nil {
@@ -60,15 +72,14 @@ func main() {
 		os.Exit(1)
 	}
 
-	writerJob := sync.WaitGroup{}
-
 	writequeue := make(chan string, 10000)
 
+	writerJob := sync.WaitGroup{}
 	writerJob.Add(1)
 	go func() {
 		defer writerJob.Done()
 
-		w := bufio.NewWriter(os.Stdout)
+		w := bufio.NewWriter(outfile)
 		defer w.Flush()
 
 		for permutation := range writequeue {
@@ -81,6 +92,10 @@ func main() {
 	jobs := sync.WaitGroup{}
 
 	var concurrency int = int(math.Round( float64(runtime.NumCPU()) * 1.5 ))
+	if verbose {
+		loge("Concurrency set to %d\n", concurrency)
+	}
+
 	for i := 0; i < concurrency; i++ {
 		jobs.Add(1)
 		go func() {
@@ -100,11 +115,7 @@ func main() {
 
 					uniq[permutation] = true
 
-					// if output == "" {
-					// 	fmt.Printf("%s", permutation)
-					// } else {
 					writequeue <- permutation
-					// }
 				}
 			}
 		}()
@@ -115,51 +126,9 @@ func main() {
 	}
 	close(hosts)
 
-	// for _, u := range hostList {
-	// 	subdomain := domainutil.Subdomain(u)
-	// 	domainSuffix := domainutil.Domain(u)
-	// 	jobs.Add(1)
-	// 	go func(domain string) {
-	// 		defer jobs.Done()
-	// 		uniq := make(map[string]bool)
-	// 		for r := range altdns.Permute(subdomain) {
-	// 			permutation := fmt.Sprintf("%s.%s\n", r, domainSuffix)
-
-	// 			// avoid duplicates
-	// 			if uniq[permutation] {
-	// 				continue
-	// 			}
-
-	// 			uniq[permutation] = true
-
-	// 			// if output == "" {
-	// 			// 	fmt.Printf("%s", permutation)
-	// 			// } else {
-	// 			writequeue <- permutation
-	// 			// }
-	// 		}
-	// 		uniq = nil
-	// 	}(u)
-	// }
-
-	// ticker := time.NewTicker(1 * time.Second)
-	// done := make(chan bool)
-	// go func() {
-	// 	for {
-	// 		select {
-	// 		case <- done:
-	// 			return
-	// 		case <-ticker.C:
-	// 			runtime.GC()
-	// 		}
-	// 	}
-	// }()
-
 	jobs.Wait()
 
 	close(writequeue)
 
 	writerJob.Wait()
-	// ticker.Stop()
-	// done <- true
 }
